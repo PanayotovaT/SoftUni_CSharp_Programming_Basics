fbaaecadd142d0732d9c4881887d1ce28142e515
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-27 18:17:44 +0200

    Added file

---
+++ Simple-Operations-and-Calculations-Exercises/dance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	L – дължина на залата в метри – реално число в интервала  [10.00 … 100.00]
+            //2.	W – ширина на залата в метри – реално число в интервала  [10.00 … 100.00]
+            //3.	А – страна на гардероба в метри – реално число в интервала [2.00… 20.00]
+            //един танцьор е 40 см² и допълнително за свободно движение му трябват  още  7000см². 
+
+            double lengthHall = double.Parse(Console.ReadLine());
+            double widthHall = double.Parse(Console.ReadLine());
+            double sideWardrobe = double.Parse(Console.ReadLine());
+            double areaPerson = 7040;
+
+            double fullAreaHall = lengthHall * 100 * widthHall * 100;
+            double benchArea = fullAreaHall / 10;
+            double areaWardrobe = sideWardrobe * 100 * sideWardrobe * 100;
+
+            double freeAreaHall = fullAreaHall - benchArea - areaWardrobe;
+
+            double capacityPeople = freeAreaHall / areaPerson;
+
+            Console.WriteLine(Math.Floor(capacityPeople));
+
+
+        }
+    }
+}
