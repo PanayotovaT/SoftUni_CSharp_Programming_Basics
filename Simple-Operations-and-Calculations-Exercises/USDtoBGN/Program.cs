4104a8bfcc9700b1ccb5d377b8b049cada75a442
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-27 16:28:36 +0200

    Added file

---
+++ Simple-Operations-and-Calculations-Exercises/USDtoBGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1ExerciseUSDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            double bgn = dollars * 1.763468;
+            Console.WriteLine($"{bgn:f2}");
+
+        }
+    }
+}
