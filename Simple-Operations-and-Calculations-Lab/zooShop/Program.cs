91e449ca0275599ae726fdceceec9a428f075ce6
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-27 16:22:04 +0200

    Added file

---
+++ Simple-Operations-and-Calculations-Lab/zooShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project_Creation_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int otherAnimals = int.Parse(Console.ReadLine());
+            double dogsExpense = dogs * 2.50;
+            double otherAnimalsExpense = otherAnimals * 4;
+            double expense = dogsExpense + otherAnimalsExpense;
+            Console.WriteLine($"{Math.Round(expense, 3)} lv.");
+
+        }
+    }
+}
