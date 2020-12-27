3a9327433b141954d6888eabb79c08d21f7b078f
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-27 15:58:06 +0200

    Added file

---
+++ Simple-Operations-and-Calculations-Lab/Inches_To_Centimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inches_To_Centimeters1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //която чете от конзолата число (не непременно цяло) и преобразува числото от инчове в сантиметри.
+            //За целта умножава инчовете по 2.54 
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine(centimeters);
+        }
+    }
+}
