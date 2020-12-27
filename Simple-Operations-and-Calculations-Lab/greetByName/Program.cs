202479592554137c41ca77eacb29fe26045bf121
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-27 16:00:26 +0200

    Added file

---
+++ Simple-Operations-and-Calculations-Lab/greetByName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inches_to_centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
