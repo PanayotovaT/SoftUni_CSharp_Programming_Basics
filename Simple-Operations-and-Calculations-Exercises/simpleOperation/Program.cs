3ce70159056ee7bc424a6be23b89d4f02e6f8065
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-27 18:21:42 +0200

    Added file

---
+++ Simple-Operations-and-Calculations-Exercises/simpleOperation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 45.347689;
+            Console.WriteLine($"{number:f2}");
+        }
+    }
+}
