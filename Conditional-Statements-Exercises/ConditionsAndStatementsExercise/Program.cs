d02f3104038db849381d7207e485e7b7e8f0f531
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-29 04:52:46 +0200

    Added file

---
+++ Conditional-Statements-Exercises/ConditionsAndStatementsExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConditionsAndStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdtime = int.Parse(Console.ReadLine());
+
+            int totalTimeInSeconds = firstTime + secondTime + thirdtime;
+            int timeInMinutes = totalTimeInSeconds / 60;
+            int timeInSeconds = totalTimeInSeconds % 60;
+
+            if (timeInSeconds < 10)
+            {
+                Console.WriteLine(timeInMinutes + ":0" + timeInSeconds);
+
+            }
+            else
+            {
+                Console.WriteLine(timeInMinutes + ":" + timeInSeconds);
+            }
+            //Console.WriteLine($"{timeInMinutes}:{timeInSeconds:D2}")
+        }
+    }
+}
