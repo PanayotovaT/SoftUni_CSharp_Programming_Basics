00b9105e0a1f999281fee01aee4d5d213362e340
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-29 04:27:26 +0200

    Added file

---
+++ Conditional-Statements-Lab/evenOrOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOrOdd_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 ==1)
+            {
+                Console.WriteLine("Even");
+
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
