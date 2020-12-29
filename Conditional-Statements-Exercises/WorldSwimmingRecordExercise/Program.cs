e4d5ae4cb8b2f4d399ac518e7e59d1f4cd617389
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-29 04:55:43 +0200

    Added file

---
+++ Conditional-Statements-Exercises/WorldSwimmingRecordExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorldSwimmingRecordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            1.Рекордът в секунди – реално число в интервала[0.00 … 100000.00]
+            //2.Разстоянието в метри – реално число в интервала[0.00 … 100000.00]
+            //3.Времето в секунди, за което плува разстояние от 1 м. - реално число в интервала[0.00 … 1000.00]
+
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double time = distance * timePerMeter;
+            double delayTime = Math.Floor(distance / 15) * 12.5;
+            double totalTime = time + delayTime;
+
+            if (totalTime < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+
+            }
+            else
+            {
+                double missingSeconds = totalTime - record;
+                    Console.WriteLine($"No, he failed! He was {missingSeconds:f2} seconds slower.");
+            }
+        }
+    }
+}
