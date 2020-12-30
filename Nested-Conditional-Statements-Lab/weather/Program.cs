14dd19feae1dfbb826062f37701ed72611ad2be8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-30 18:48:31 +0200

    Added file

---
+++ Nested-Conditional-Statements-Lab/weather/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weather = Console.ReadLine();
+
+            //if (weather == "cloudy")
+            //{
+            //    Console.WriteLine("Take an umbrella");
+
+            //}
+            //else if (weather == "sunny")
+            //{
+            //    Console.WriteLine("Go out for a walk");
+            //}
+            //else if (weather == "windy")
+            //{
+            //    Console.WriteLine("Take a jacket");
+            //}
+            //else if (weather == "snowy")
+            //{
+            //    Console.WriteLine("Go skiing.");
+            //}
+            //else if (weather == "rainy")
+            //{
+            //    Console.WriteLine("Take an umbrella");
+            //}
+
+            switch (weather)
+            {
+                case "cloudy":
+                    Console.WriteLine("Take an umbrella");
+                    break;
+                case "sunny":
+                    Console.WriteLine("Go out for a walk");
+                    break;
+                case "windy":
+                    Console.WriteLine("Take a jacket");
+                    break;
+                case "snowy":
+                    Console.WriteLine("Go skiing.");
+                    break;
+                case "rainy":
+                    Console.WriteLine("Take an umbrella");
+                    break;
+                default:
+                    Console.WriteLine("No data available.");
+                    break;
+            }
+        }
+    }
+}
