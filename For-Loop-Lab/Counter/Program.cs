e0a4f5f5469065b57a162592a8bb281ca75b79bd
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-31 00:37:21 +0200

    Added file

---
+++ For-Loop-Lab/Counter/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (int counter = 0; counter < 5; counter +=1)
+            //{
+            //    Console.WriteLine("Summer");
+            //    Console.WriteLine("Autumn");
+            //    Console.WriteLine("Winter");
+            //    Console.WriteLine("Spring");
+            //}
+
+            //for (int i = 1; i <= 10; i++)
+
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int year = 2020; year <= 2024; year++)
+            //{
+            //       Console.WriteLine(year);
+            //       Console.WriteLine("Summer");
+            //       Console.WriteLine("Autumn");
+            //       Console.WriteLine("Winter");
+            //       Console.WriteLine("Spring");
+            //}
+
+            //for (int i = 1; i < 10; i +=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (; ; )
+            //{
+            //    Console.WriteLine(10);
+            //}
+
+            //for (int i = 1; i <= 10; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 15; i >=1; i -= 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(double.MinValue);
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    string name = Console.ReadLine();
+            //}
+            double num = Math.Pow(2, 4);
+            Console.WriteLine(num);
+        }
+
+    }
+}
