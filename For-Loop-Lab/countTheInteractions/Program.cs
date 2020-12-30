8addebe3e157fea95cbd3b718621eb22a02f1a50
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-31 00:38:12 +0200

    Added file

---
+++ For-Loop-Lab/countTheInteractions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace countTheInteractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int itarationCounter = 1;
+            for (int i = 1; i <= 15; i += 2)
+            {
+                Console.WriteLine($"{itarationCounter} - {i}");
+                
+                itarationCounter++;
+            }
+            Console.WriteLine(itarationCounter - 1);
+        }
+    }
+}
