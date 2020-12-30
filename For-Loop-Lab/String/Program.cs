55ba2b7e0bd8ce1c7e92105848851429adc30909
fortalesi <75318821+fortalesi@users.noreply.github.com>
2020-12-31 00:40:45 +0200

    Added file

---
+++ For-Loop-Lab/String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Teodora&Alex";
+            Console.WriteLine(name.Length);
+
+
+            string nome = "Sofia";
+            Console.WriteLine("My name is {0}",nome);
+
+
+            string prenom = "Alexander";
+                           //012345678
+            Console.WriteLine(prenom[3]);
+
+
+            for (int i = 0; i <= 8; i++)
+            {
+                Console.WriteLine(prenom[i]);
+            }
+            
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+
+
+
+        }
+    }
+}
