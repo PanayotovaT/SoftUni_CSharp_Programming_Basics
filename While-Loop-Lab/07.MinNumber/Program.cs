e5b84662a25a79c322b95cf0346ba2b4def1c216
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:01:46 +0200

    Added file

---
+++ While-Loop-Lab/07.MinNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            int minNumber = int.MaxValue;
+
+            while (i <= n)
+            {
+                int number = int.Parse(Console.ReadLine());
+                i++;
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
