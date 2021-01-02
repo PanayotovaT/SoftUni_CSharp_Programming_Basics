446190fbd742774e6e7409bde79fed0e2f7b37de
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:05:18 +0200

    Added file

---
+++ While-Loop-Lab/moving/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            
+            int freeSpace = width * length * height;
+            string boxes = Console.ReadLine();
+            while (boxes != "Done")
+            {
+                freeSpace -= int.Parse(boxes);
+
+                if (freeSpace < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+                    break;
+                }
+
+                boxes = Console.ReadLine();
+            }
+            if (boxes == "Done")
+            {
+                Console.WriteLine($"{freeSpace} Cubic meters left.");
+            }
+        }
+    }
+}
