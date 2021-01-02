4d82decee30fe556476f2131b35a239322a1b9de
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:10:39 +0200

    Added file

---
+++ While-Loop-Lab/sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+
+            while (input != "Stop")
+
+            {
+                sum += double.Parse(input);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
