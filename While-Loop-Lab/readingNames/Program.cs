80b006530c1a90abc9bb44adb657c52559f4e58f
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:09:47 +0200

    Added file

---
+++ While-Loop-Lab/readingNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Reading_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            int counter = 0;
+
+            while(text != "Stop")
+            {
+                text = Console.ReadLine();
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
