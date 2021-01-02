c0d563fda5864269b4e7fa13875e43c834c5a5ce
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 21:32:28 +0200

    Added file

---
+++ While-Loop-Lab/05.Balance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int transactions = int.Parse(Console.ReadLine());
+            double sum = 0;
+            int i = 1;
+            while (i <= transactions)
+            {
+                double money = double.Parse(Console.ReadLine());
+                if (money < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                sum += money;
+
+                Console.WriteLine($"Increase: {money:f2}");
+
+                i++;
+
+            }
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
