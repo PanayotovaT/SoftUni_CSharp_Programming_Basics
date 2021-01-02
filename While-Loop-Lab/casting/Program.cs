46f24d7f726a119c4b565da51c5998dfe97dcc44
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:04:18 +0200

    Added file

---
+++ While-Loop-Lab/casting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5.5;
+            int b = (int)a;
+            Console.WriteLine(b);
+        }
+    }
+}
