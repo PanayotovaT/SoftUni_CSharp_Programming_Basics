9e32c6abae8cb87f22ae732ff3014c069bc86ed0
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:02:34 +0200

    Added file

---
+++ While-Loop-Lab/08.Graduation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08.Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int n = 1;
+            double sumGrades = 0;
+            double averageGrade = 0;
+           
+            while(n <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if(grade < 4)
+                {
+                    continue;
+                }
+                n++;
+
+                sumGrades += grade;
+                averageGrade = sumGrades / 12;
+
+
+              
+            }
+            Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+        }
+    }
+}
