a18e8516c1266245606b5cf4c386a86788217517
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 21:33:20 +0200

    Added file

---
+++ While-Loop-Lab/06.MaxNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            int maxNum = int.MinValue;
+            
+            while (i <= n)
+            {
+                int number = int.Parse(Console.ReadLine());
+                i++;
+                
+                
+                if(number > maxNum)
+                {
+                    maxNum = number;
+                }
+
+
+
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
