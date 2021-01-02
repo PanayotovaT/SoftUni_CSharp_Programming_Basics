8d2bed8a0c9618a9baf04ae60dbc6f8e42fb198b
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-02 22:03:23 +0200

    Added file

---
+++ While-Loop-Lab/09.Graduation.Part2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _09.Graduation.Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double averageGrade = 0;
+            int classN = 1;
+            int counter = 1;
+            double sumGrade = 0;
+            while(classN <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                
+                if (grade < 4 && counter < 2)
+                {
+                    counter++;
+                    continue;
+                }
+                else if (grade < 4 && counter == 2)
+                {
+                    Console.WriteLine($"{name} has been excluded at {classN} grade");
+                    break;
+                }
+                classN++;
+                
+                sumGrade += grade;
+            }
+
+            if (counter != 2)
+            {
+                averageGrade = sumGrade / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+            }
+
+
+        }
+    }
+}
