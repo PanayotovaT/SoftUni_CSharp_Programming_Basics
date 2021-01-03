ad0a787c6e7fce321fa5230c2dd0881000ead5da
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-03 04:47:16 +0200

    Added file

---
+++ While-Loop-Exercises/library/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedBookTitle = Console.ReadLine();
+            int totalBooks = int.Parse(Console.ReadLine());
+
+            int checkedBooks = 0;
+
+            while( totalBooks > 0)
+
+            {
+                string bookTitle = Console.ReadLine();
+
+                if (bookTitle == searchedBookTitle)
+                {
+                    Console.WriteLine($"You checked {checkedBooks} books and found it.");
+                    break;
+                }
+                else
+                {
+                    checkedBooks++;
+                }
+                totalBooks--;
+
+            }
+            if(totalBooks <=0)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {checkedBooks} books.");
+            }
+        }
+    }
+}
