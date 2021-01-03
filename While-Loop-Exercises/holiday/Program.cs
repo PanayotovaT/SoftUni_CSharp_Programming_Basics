c2e30b71b2ba174e6d1b65fa526e5b41c380ce84
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-03 04:46:21 +0200

    Added file

---
+++ While-Loop-Exercises/holiday/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Holiday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            double availableMoney = double.Parse(Console.ReadLine());
+            int countDays = 0;
+            int countSpendDays = 0;
+
+
+            while (availableMoney < tripPrice)
+            {
+                string action = Console.ReadLine(); //spend, save
+                countDays++;
+
+                if (action == "save")
+                {
+                    double saveMoney = double.Parse(Console.ReadLine());
+                    availableMoney += saveMoney;
+                    countSpendDays = 0;
+                }
+                else if (action == "spend")
+                {
+                    double spendMoney = double.Parse(Console.ReadLine());
+                    countSpendDays++;
+                    if (spendMoney > availableMoney)
+                    {
+                        availableMoney = 0;
+                    }
+                    else if (spendMoney <= availableMoney)
+                    {
+                        availableMoney -= spendMoney;
+                    }
+                    if (countSpendDays == 5)
+                    {
+                        Console.WriteLine("You can't save the money.");
+                        Console.WriteLine(countDays);
+                        break;
+                    }
+
+                }
+                if (availableMoney >= tripPrice)
+                {
+                    Console.WriteLine($"You saved the money for {countDays} days.");
+                }
+            }
+        }
+    }
+}
