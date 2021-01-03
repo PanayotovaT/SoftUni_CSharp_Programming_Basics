bb0526a7fdd92b2e0466bb628911a986d242c02b
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-03 04:41:21 +0200

    Added file

---
+++ While-Loop-Exercises/04.Steps/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.Steps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int steps = 0;
+            
+
+            while (steps < 10000)
+            {
+                string command = Console.ReadLine();
+                if (command != "Going home")
+                {
+                    int stepsToHome = int.Parse(command);
+                    steps += stepsToHome;
+                }
+                else if (command == "Going home")
+                {
+                    int walkedStepsHome = int.Parse(Console.ReadLine());
+                    steps += walkedStepsHome;
+                    break;
+                }
+
+            }
+            if (steps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+            }
+            else 
+            {
+                int finalsteps = 10000 - steps;
+                Console.WriteLine($"{finalsteps} more steps to reach goal.");
+            }
+        }
+    }
+}
