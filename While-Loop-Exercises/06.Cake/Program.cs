ff7a62d5862ca925936290324a77c28063c5ca15
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-03 04:42:29 +0200

    Added file

---
+++ While-Loop-Exercises/06.Cake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeLength = int.Parse(Console.ReadLine());
+            int cakeSize = cakeWidth * cakeLength;
+            
+            int takenPieces = 0;
+            string command = "";
+
+            while (cakeSize > 0)
+            {
+                command = Console.ReadLine();
+                
+                if (command == "STOP") 
+                {
+                    break;
+                }
+                else if (command != "STOP")
+                {
+                    takenPieces = int.Parse(command);
+                    cakeSize -= takenPieces;
+                }
+                
+
+
+            }
+            if (command == "STOP")
+            {
+                Console.WriteLine($"{cakeSize} pieces are left.");
+            }
+            else if (cakeSize <= 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakeSize)} pieces more.");
+            }
+        }
+    }
+}
