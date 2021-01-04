9c3687e715b823176efc96f387bf0c8b2ecfd19d
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-04 06:20:41 +0200

    Added file

---
+++ Nested-Loops-Lab/interruptLoops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterruptLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int minutes = 0; minutes <= 59; minutes++)
+                {
+                    Console.WriteLine($"{hour}:{minutes}");
+                    if (hour == 2 && minutes == 37)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+            //Друг начин за прекъсване
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int minutes = 0; minutes <= 59; minutes++)
+                {
+                    Console.WriteLine($"{hour}:{minutes}");
+                    if (hour == 0 && minutes == 37)
+                    {
+                        return; //приключва цялата програма == environment.Exit(0);
+                    }
+
+                }
+            }
+        }
+
+
+    }
+}
+
