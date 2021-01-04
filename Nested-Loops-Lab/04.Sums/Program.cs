f374e56b6de58242d3ec9b93129c4b7a4a846f51
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-04 06:22:25 +0200

    Added file

---
+++ Nested-Loops-Lab/04.Sums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04.Sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+
+            for (int x = startNum; x <= endNum; x++)
+            {
+                int sum = 0;
+                for (int y = startNum; y <= endNum; y++)
+                {
+                    sum = x + y;
+                    counter++;
+                    if (sum == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({x} + {y} = {magicNum})");
+                        return;
+                    }
+
+                }
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {magicNum}");
+
+        }
+    }
+}
