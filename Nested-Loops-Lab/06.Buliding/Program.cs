0c45471f71da81f11d1058a71f83487bf3fcc176
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-04 06:23:48 +0200

    Added file

---
+++ Nested-Loops-Lab/06.Buliding/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06.Buliding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int etageNum = int.Parse(Console.ReadLine());
+            int roomsNum = int.Parse(Console.ReadLine());
+
+            for (int n = etageNum; n >= 1; n--)
+            {
+                string roomN = "";
+                for (int j = 0; j < roomsNum; j++)
+                {
+                    if (n == etageNum)
+                    {
+                        roomN += String.Format($"L{n}{j} ");
+                        
+                    }
+                    else
+                    {
+                        if (n % 2 == 0)
+                        {
+                            roomN += String.Format($"O{n}{j} ");
+                        }
+                        else
+                        {
+                            roomN += String.Format($"A{n}{j} ");
+                        }
+                    }
+                }
+               Console.WriteLine(roomN);
+            }
+        }
+    }
+}
