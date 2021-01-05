b00a04da084618bd792c4a633d489bbf1d9c932a
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-05 09:44:52 +0200

    Added file

---
+++ Nested-Loops-Exercises/02.EqualSumEvenOddPositions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.EqualSumEvenOddPositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            for (int i = number1; i <= number2; i++)
+            {
+                string currentNum = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+
+                for (int position = 0; position < currentNum.Length; position++)
+                {
+                    int currentDigit = int.Parse(currentNum[position].ToString());
+                    if (position % 2 == 0)
+                        evenSum += currentDigit;
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+
+                if(oddSum == evenSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
