e470dc036bd0870c9ed0b9b58d53230feb45e4f7
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-08 15:49:55 +0200

    Added file

---
+++ exercises/06.Tournament/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _06.Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int countWinDays = 0;
+            int countLoseDays = 0;
+            double totalWin = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                double gameWin = 0;
+                int countWin = 0;
+                int countLost = 0;
+
+                string sport = Console.ReadLine();
+                string result = Console.ReadLine();
+                while (sport != "Finish")
+                {
+                    if (result == "win")
+                    {
+                        gameWin += 20;
+                        countWin++;
+
+                    }
+                    else if (result == "lose")
+                    {
+                        countLost++;
+                    }
+
+                    sport = Console.ReadLine();
+                    if (sport != "Finish")
+                          { result = Console.ReadLine(); }
+                }
+                if (countWin > countLost)
+                {
+                    gameWin += 0.10 * gameWin;
+                    countWinDays++;
+                }
+                else if (countWin < countLost)
+                {
+                    countLoseDays++;
+                }
+                totalWin += gameWin;
+            }
+            if (countWinDays > countLoseDays)
+            {
+                totalWin += 0.20 * totalWin;
+                Console.WriteLine($"You won the tournament! Total raised money: {totalWin:f2}");
+            }
+            else if (countWinDays < countLoseDays)
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {totalWin:f2}");
+            }
+        }
+    }
+}
