54c383378513aca94a06e9ff1295acc8b48332ba
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-08 15:48:21 +0200

    Added file

---
+++ exercises/05.CareOfPuppy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	Закупеното количество храна за кученцето в килограми – цяло число в интервала[1 …100]
+            //•	На всеки следващ ред до получаване на команда Adopted ще получавате колко грама изяжда кученцето на всяко хранене -цяло число в интервала[10 …1000]
+
+            int allFood = int.Parse(Console.ReadLine());
+            string consumedFood = Console.ReadLine();
+            int allFoodInGrams = allFood * 1000;
+            int consumedFoodInGrams = 0;
+            while (consumedFood != "Adopted")
+            {
+
+                consumedFoodInGrams += int.Parse(consumedFood);
+                
+                consumedFood = Console.ReadLine();
+            }
+
+            if(consumedFoodInGrams <= allFoodInGrams)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {allFoodInGrams - consumedFoodInGrams} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {consumedFoodInGrams - allFoodInGrams} grams more.");
+            }
+        }
+    }
+}
