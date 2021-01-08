d5c51a8934454ebdffa91f0db874fe9c22873adc
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-08 16:09:14 +0200

    Added file

---
+++ examExercises/exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Ceiling(2.3565));
+        }
+    }
+}
