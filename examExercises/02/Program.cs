04f023ae2a6a377ebc2df0c33f2bc5645c2fa705
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-08 16:02:32 +0200

    Added file

---
+++ examExercises/02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 2;
+            int value2 = 3;
+
+            for (int i = 0; i < 33; i++)
+            {
+                Console.WriteLine($"{value}^{i} = {Math.Pow(value,i)}            {value2}^{i} = {Math.Pow(value2, i)}");
+
+                
+
+            }
+        }
+    }
+}
