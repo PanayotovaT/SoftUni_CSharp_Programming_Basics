9584562eb7739333944bd05b5ddac3be939c7345
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-10 01:07:45 +0200

    Added file

---
+++ Exam/01.HoneyCombs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exam07._06._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int numOfBees = int.Parse(Console.ReadLine());
+            int numofFlowers = int.Parse(Console.ReadLine());
+
+            double producedHoney = Math.Round((numOfBees * numofFlowers * 0.21), 3);
+            double honeyComb = Math.Floor(producedHoney / 100);
+            double leftHoney = Math.Round((producedHoney % 100), 3);
+
+            Console.WriteLine($"{honeyComb} honeycombs filled.");
+            Console.WriteLine($"{leftHoney:f2} grams of honey left.");
+
+        }
+    }
+}
