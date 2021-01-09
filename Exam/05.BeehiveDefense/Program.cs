c9b5688895c9e02adf44bad4144c892a420e01ab
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-10 01:05:44 +0200

    Added file

---
+++ Exam/05.BeehiveDefense/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.BeehiveDefense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            •	На първи ред получавате брой пчели: цяло число[100 - 10000].
+            //•	На втори ред получавате здраве: цяло число[1 - 10000].
+            //•	На трети ред получавате атака: цяло число[1 - 10000].
+
+            int numOfBees = int.Parse(Console.ReadLine());
+            int health = int.Parse(Console.ReadLine());
+            int attck = int.Parse(Console.ReadLine());
+
+            while(numOfBees >= 0)
+            {
+
+                numOfBees -= attck;
+                if(numOfBees < 0)
+                {
+                    Console.WriteLine("The bear stole the honey! Bees left 0.");
+                }
+                else if(numOfBees < 100)
+                {
+                    Console.WriteLine($"The bear stole the honey! Bees left {numOfBees}.");
+                    break;
+                }
+                health -= numOfBees * 5;
+                if(health <= 0)
+                {
+                    Console.WriteLine($"Beehive won! Bees left {numOfBees}.");
+                    break;
+                }
+
+
+            }
+
+        }
+    }
+}
